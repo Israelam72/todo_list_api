5b063ebaa318b187d61dd998945d3eb2eb8f3c6b
Israel Arantes <70655210+Israelam72@users.noreply.github.com>
2024-11-15 23:49:11 -0300

    Atualizar o views.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world.You're at the polls index.")
+
+def example(request):
+    return HttpResponse("teste")
